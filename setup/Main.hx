4d896592716d6cc358821a38a28cf578972c709a
TheLeerName <lesha.nechaev.05@mail.ru>
2024-01-01 23:11:30 +0700

    building guide and todo list

--- setup/Main.hx
+++ setup/Main.hx
@@ -12,14 +12,15 @@ typedef Library = {
 
 class Main {
 	public static function main():Void {
-		// Create a folder to prevent messing with hmm libraries
-		if (!FileSystem.exists(".haxelib"))
-			FileSystem.createDirectory(".haxelib");
-
 		// brief explanation: first we parse a json containing the library names, data, and such
 		final libs:Array<Library> = Json.parse(File.getContent('./hmm.json')).dependencies;
 
+		#if windows
+		Sys.command('color 0a');
+		#end
+
 		// now we loop through the data we currently have
+		Sys.println('Installing dependencies.');
 		for (data in libs) {
 			// and install the libraries, based on their type
 			switch (data.type) {
@@ -33,6 +34,14 @@ class Main {
 					Sys.println('[PSYCH ENGINE SETUP]: Unable to resolve library of type "${data.type}" for library "${data.name}"');
 			}
 		}
+
+		#if windows
+		if (!FileSystem.exists('./vs_Community.exe'))
+			Sys.command('curl -# -O https://download.visualstudio.microsoft.com/download/pr/3105fcfe-e771-41d6-9a1c-fc971e7d03a7/8eb13958dc429a6e6f7e0d6704d43a55f18d02a253608351b6bf6723ffdaf24e/vs_Community.exe');
+		Sys.command('vs_Community.exe --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK.19041 -p');
+		#end
+
+		Sys.println('Finished!');
 
 		// after the loop, we can leave
 		Sys.exit(0);
