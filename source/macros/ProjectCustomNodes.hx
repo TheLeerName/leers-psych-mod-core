515535560cee48a8e3f9a3db407dfdf9cd3b0c57
TheLeerName <lesha.nechaev.05@mail.ru>
2024-01-02 02:07:32 +0700

    assets_windows node project.xml
    get rid of manifest folder part 1

---
+++ source/macros/ProjectCustomNodes.hx
@@ -0,0 +1,49 @@
+package macros;
+
+#if macro
+import haxe.macro.Compiler;
+import haxe.macro.Context;
+
+import sys.io.File;
+
+class ProjectCustomNodes {
+	public static function init() {
+		var output:String = Compiler.getOutput();
+		output = output.substring(0, output.lastIndexOf('/') + 1) + 'bin';
+
+		function defined(el:Xml) {
+			var v:Bool = true;
+			if (el.get('unless') != null)
+				v = !Context.defined(el.get('unless'));
+			if (el.get('if') != null)
+				v = Context.defined(el.get('if'));
+
+			return v;
+		}
+
+		function no(elements:Iterator<Xml>) {
+			for (el in elements) {
+				switch(el.nodeName) {
+					#if windows
+					case 'assets_windows':
+						if (!defined(el)) continue;
+						var path:String = el.get('path');
+						if (path == null) continue;
+
+						var rename:String = el.get('rename');
+						rename = rename != null && rename.length > 0 ? '/$rename' : '/$path';
+
+						Sys.command('robocopy "$path" "${output + rename}" /S /NP /NFL /NDL /NJH /NJS');
+					#end
+					case 'section':
+						if (defined(el))
+							no(el.elements());
+				}
+			}
+		}
+
+		var xml = Xml.parse(File.getContent('Project.xml'));
+		no(xml.firstElement().elements());
+	}
+}
+#end
