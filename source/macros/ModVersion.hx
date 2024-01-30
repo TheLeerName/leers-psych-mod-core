ce1be802682ba79b493d4edba24c52ab9df8e68f
Leer <85291330+TheLeerName@users.noreply.github.com>
2024-01-31 03:31:50 +0700

    fix of modVersion macro

--- source/macros/ModVersion.hx
+++ source/macros/ModVersion.hx
@@ -8,25 +8,28 @@ class ModVersion {
 	public static function add():Array<Field> {
 		var curDate = Date.now();
 		var ymd = DateTools.format(curDate, "%Y%m%d");
-		var buildDir = #if debug 'export/debug/' #else 'export/release/' #end;
-		if (!FileSystem.exists(buildDir))
-			FileSystem.createDirectory(buildDir);
+		var buildNumber:Int = 0;
 
-		var buildNumber:Int = 1;
-		if (FileSystem.exists(buildDir + '.build'))
-			buildNumber = Std.parseInt(File.getContent(buildDir + '.build'));
+		if (Compiler.getOutput() != '_') {
+			var buildDir = #if debug 'export/debug/' #else 'export/release/' #end;
+			if (!FileSystem.exists(buildDir))
+				FileSystem.createDirectory(buildDir);
 
-		if (!FileSystem.exists(buildDir + '.builddate'))
-			File.saveContent(buildDir + '.builddate', ymd + '');
-		var buildDate = File.getContent(buildDir + '.builddate');
-		if (buildDate != ymd) {
-			File.saveContent(buildDir + '.build', '1');
-			FileSystem.deleteFile(buildDir + '.builddate');
-			buildNumber = 1;
+			var build = File.getContent(buildDir + '.builddate').split('\n') ?? [ymd, buildNumber + ''];
+
+			buildNumber = Std.parseInt(build[1]) ?? 0;
+			if (build[0] == ymd)
+				buildNumber++;
+			else {
+				buildNumber = 0;
+				build[0] = ymd;
+			}
+
+			File.saveContent(buildDir + '.builddate', build[0] + '\n' + buildNumber);
+
+			// its like yearmonthday.build_number
+			trace('$ymd.$buildNumber');
 		}
-
-		// its like yearmonthday.build_number
-		trace('$ymd.$buildNumber');
 		return MacroUtils.addString('modVersion', '$ymd.$buildNumber', 'public static');
 	}
 }
